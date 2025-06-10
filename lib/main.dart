1bc7d1754aac26b341f694a77a21bdcd22f1587c
iamsalahudin <iamsalahudin123@gmail.com>
2025-06-10 12:33:21 +0500

    resolved error related to .env

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'firebase_options.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
